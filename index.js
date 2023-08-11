7f153d8bffb5e9f964d882b6b8f53bd974c06311
Danijel Mitrovic <danijel.mitrovic@hotmail.com>
2023-08-11 10:51:21 +0200

    Added name attribute and attrigen func params

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-module.exports = function attrigen() {
+module.exports = function attrigen(args) {
+    const process = function (id) {
+        const { map } = args;
+        if (map) {
+            for (const [key, value] of Object.entries(map)) {
+                if (key == id) return value;
+            }
+        }
+        return false;
+    }
+
     const generate = function (element) {
-        const attributeName = "data-test-id";
+        const attributeId = "data-test-id";
+        const attributeName = "data-test-name";
         const elementChildren = element.children;
         var elementIndex = 1;
         for (const elementChild of elementChildren) {
             var valuePrefix = "";
-            const parentAttributeValue = elementChild.parentElement.getAttribute(attributeName);
-            if (parentAttributeValue) {
-                valuePrefix = parentAttributeValue;
+            const parentAttributeIdValue = elementChild.parentElement.getAttribute(attributeId);
+            if (parentAttributeIdValue) {
+                valuePrefix = parentAttributeIdValue;
             }
-            var attributeValue = valuePrefix + elementChild.localName.charAt(0) + elementIndex;
-            elementChild.setAttribute(attributeName, attributeValue);
+            var attributeIdValue = valuePrefix + elementChild.localName.charAt(0) + elementIndex;
+            elementChild.setAttribute(attributeId, attributeIdValue);
+            var attributeNameValue = process(attributeIdValue);
+            if (attributeNameValue) {
+                elementChild.setAttribute(attributeName, attributeNameValue);
+            }
             elementIndex++;
             generate(elementChild);
         }
